64a5d1ef9cddf86f10b583a92e6d54d06f97ddae
Joseph_Berro <berr0107@algonquincollege.com>
2019-11-22 21:36:28 -0500

    project commit

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private TextView textView2;
+    private EditText editText;
+    private EditText editText2;
+    private Button saveButton;
+
+    public static final String SHARED_PREFS = "sPrefs";
+    public static final String TEXT = "text";
+    private String text;
+    private String text2;
+    public SharedPreferences prefs;
+
+
+
+    @Override
+    public SharedPreferences getPreferences(int mode) {
+        return super.getPreferences(mode);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
