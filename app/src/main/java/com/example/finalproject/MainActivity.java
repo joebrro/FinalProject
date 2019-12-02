1c113bbfc81e917b5d76957f84206eb2764e52b2
Joseph_Berro <berr0107@algonquincollege.com>
2019-12-02 14:18:04 -0500

    project commit

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,11 +2,39 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +50,12 @@ public class MainActivity extends AppCompatActivity {
     private String text2;
     public SharedPreferences preferences;
 
-
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.recipe_menu_page, menu);
+        return true;
+    }
     @Override
     public SharedPreferences getPreferences(int mode) {
         return super.getPreferences(mode);
@@ -34,6 +66,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button food = findViewById(R.id.search_recipe);
+        if(food != null) {
+            food.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View clk) {
+                    Intent goToRecipePage = new Intent(MainActivity.this, RecipePageActivity.class);
 
+                    MainActivity.this.startActivityForResult(goToRecipePage, 40);
+                }
+            });
+        }
     }
 }
